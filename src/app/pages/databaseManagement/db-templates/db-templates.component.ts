c73890ef3a109ba98c3f1123ce9ba050f3480aff
dayan <eam.dayan@gmail.com>
2023-05-29 23:32:41 +0700

    set profiles for db templates

--- src/app/pages/databaseManagement/db-templates/db-templates.component.ts
+++ src/app/pages/databaseManagement/db-templates/db-templates.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DatabaseRequest } from 'src/app/models/database';
 import { DatabaseService } from 'src/app/services/database/database.service';
 
-type vendors = 'postgres' | 'mongodb'
+type vendors = 'postgres' | 'mongodb';
+type profiles = 'custom' | 'default' | 'random';
+
+// type dbRequest = { containerName: string | null; defaultDatabase: string | null; username: string | null; password: string | null; port: number | null; };
 
 @Component({
   selector: 'app-db-templates',
   templateUrl: './db-templates.component.html',
   styleUrls: ['./db-templates.component.css']
 })
-export class DbTemplatesComponent {
+export class DbTemplatesComponent implements OnInit {
 
   availableVendors = ['postgres', 'mongodb']
   selectedVendor: 'postgres' | 'mongodb' = "postgres";
+  // private customProfile: Partial<dbRequest> = {};
 
-  constructor(private formBuilder: FormBuilder, private databaseService: DatabaseService, private router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private databaseService: DatabaseService,
+    private router: Router
+  ) { }
 
   container = this.formBuilder.group({
     containerName: new FormControl(''),
     defaultDatabase: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl(''),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
     port: new FormControl(3000),
   })
+
+
+  ngOnInit() {
+    // this.container.valueChanges.subscribe(val => {
+    //   this.customProfile = val
+    // })
+  }
 
   selectVendor(selected: vendors) {
     this.selectedVendor = selected
@@ -39,9 +54,54 @@ export class DbTemplatesComponent {
       password: this.container.value.password!,
       port: this.container.value.port!,
     }
-    this.databaseService.createDatabase(request).subscribe(() => {
+    if (!this.container.hasError) {
+      this.databaseService.createDatabase(request).subscribe(() => {
+        this.router.navigate(["/databases"])
+      });
+    }
+  }
 
-    });
-    this.router.navigate(["/database"])
+  setProfile(profile: profiles) {
+    switch (profile) {
+      case "default": {
+        this.container.setValue({
+          containerName: '',
+          defaultDatabase: 'postgres',
+          username: 'postgres',
+          password: 'postgres',
+          port: 3000
+        })
+        break;
+      }
+      case "random": {
+        this.container.setValue({
+          containerName: 'todo: random generate',
+          defaultDatabase: 'todo: random generate',
+          username: 'todo: random generate',
+          password: 'todo: random generate',
+          port: 3000
+        })
+        break;
+      }
+      // case "custom": {
+      //   this.container.setValue({
+      //     containerName: this.customProfile.containerName!,
+      //     defaultDatabase: this.customProfile.defaultDatabase!,
+      //     username: this.customProfile.username!,
+      //     password: this.customProfile.password!,
+      //     port: this.customProfile.port!,
+      //   })
+      //   break;
+      // }
+    }
+  }
+  clearProfile() {
+    this.container.setValue({
+      containerName: '',
+      defaultDatabase: '',
+      username: '',
+      password: '',
+      port: 3000
+    })
   }
 }
