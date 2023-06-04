c088562c1ea0c0c7048b2cdad8bfe15019f8abd2
dayan <eam.dayan@gmail.com>
2023-06-04 22:17:49 +0700

    refactor path config

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IconModule } from '@coreui/icons-angular';
-import { AuthModule } from "@app-modules/auth/auth.module";
+import { AuthModule } from "@modules/auth/auth.module";
 import { CoreRoutingModule } from './core-routing.module';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
